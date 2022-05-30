b8c9991ab70e7df438e4147993653d3e1b8bebfa
4ndr1sw <4ndr1sw@gmail.com>
2022-05-30 07:13:06 +0700

    change to instalasi listrik

--- models/Instalasi_listrik_model.php
+++ models/Instalasi_listrik_model.php
@@ -2,7 +2,7 @@
 
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class Listrik_model extends App_Model
+class Instalasi_listrik_model extends App_Model
 {
     public function __construct()
     {
@@ -11,7 +11,7 @@ class Listrik_model extends App_Model
      }
 
     /**
-     * Get listrik/s
+     * Get instalasi_listrik/s
      * @param mixed $id bucket id
      * @param array $where perform where
      * @return mixed
@@ -20,12 +20,12 @@ class Listrik_model extends App_Model
     {
         if (is_numeric($id)) {
             $this->db->where('staffid', $id);
-            $category = $this->db->get(db_prefix() . 'listrik')->row();
+            $category = $this->db->get(db_prefix() . 'instalasi_listrik')->row();
 
             return $category;
         }
-        $this->db->select('*,' . db_prefix() . 'listrik.id');
-        $this->db->from(db_prefix() . 'listrik');
+        $this->db->select('*,' . db_prefix() . 'instalasi_listrik.id');
+        $this->db->from(db_prefix() . 'instalasi_listrik');
         $this->db->where($where);
         $results = $this->db->get()->result_array();
         return $results;
@@ -34,31 +34,31 @@ class Listrik_model extends App_Model
     public function create_or_update($data, $rel_id){
         $this->db->select('id');
         $this->db->where('rel_id', $rel_id);
-        $exists = $this->db->get(db_prefix() . 'listrik')->result();
+        $exists = $this->db->get(db_prefix() . 'instalasi_listrik')->result();
         if($exists){
             $this->db->where('rel_id', $rel_id);
-            $this->db->update(db_prefix() . 'listrik', $data);
+            $this->db->update(db_prefix() . 'instalasi_listrik', $data);
         }else{
             $data['rel_id'] = $rel_id;
-            $this->db->insert(db_prefix().'listrik', $data);
+            $this->db->insert(db_prefix().'instalasi_listrik', $data);
         }
     }
 
     public function create($data){
-        $data['jenis_pesawat'] = 'listrik';
+        $data['jenis_pesawat'] = 'instalasi_listrik';
         $data['regulasi'] = get_option('predefined_regulation_of_iil');
 
-        $this->db->insert(db_prefix().'listrik', $data);
+        $this->db->insert(db_prefix().'instalasi_listrik', $data);
         $equipment_id = $this->db->insert_id();
 
-        hooks()->do_action('after_listrik_added', $equipment_id);
+        hooks()->do_action('after_instalasi_listrik_added', $equipment_id);
         return $equipment_id;
     }
 
     public function update($data, $rel_id){
         $this->db->select('id');
         $this->db->where('rel_id', $rel_id);
-        $this->db->update(db_prefix() . 'listrik', $data);
+        $this->db->update(db_prefix() . 'instalasi_listrik', $data);
     }
 
 }
--- views/admin/equiptment/instalasi_listrik_template.php
+++ views/admin/equiptment/instalasi_listrik_template.php
@@ -215,20 +215,6 @@
                   <?php echo render_textarea('equipment[kesimpulan]','equipment_kesimpulan',$value); ?>
                </div>
             </div>
-
-<!--
-<div class="btn-group">
-  <input type="radio" class="btn-check" name="options" id="option1" autocomplete="off" checked />
-  <label class="btn btn-secondary" for="option1">Checked</label>
-
-  <input type="radio" class="btn-check" name="options" id="option2" autocomplete="off" />
-  <label class="btn btn-secondary" for="option2">Radio</label>
-
-  <input type="radio" class="btn-check" name="options" id="option3" autocomplete="off" />
-  <label class="btn btn-secondary" for="option3">Radio</label>
-</div>
--->
-
          </div> <!-- end tabpanel-->
          <div role="tabpanel" class="tab-pane" id="inspection_general_data">
 
