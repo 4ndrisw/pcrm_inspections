2350e28574e4551434a57e802fcd3689f8d18752
4ndr1sw <4ndr1sw@gmail.com>
2022-07-20 01:42:36 +0700

    petir

--- install/wheel_loader.php
+++ install/wheel_loader.php
@@ -19,7 +19,7 @@ if (!$CI->db->table_exists(db_prefix() . 'wheel_loader')) {
       `pemeriksaan_visual` tinyint(1) DEFAULT NULL,
       `pemeriksaan_pengaman` tinyint(1) DEFAULT NULL,
       `pengujian_penetrant` tinyint(1) DEFAULT NULL,
-      `pengujian_operasional` tinyint(1) DEFAULT NULL,   
+      `pengujian_operasional` tinyint(1) DEFAULT NULL,
       `kesimpulan` text DEFAULT NULL,
       `temuan` text DEFAULT NULL,
       `regulasi` text DEFAULT NULL
