78370dc8974ab02cf76c1facfc11d2a8110c0802
4ndr1sw <4ndr1sw@gmail.com>
2022-09-16 07:12:08 +0700

    Update Compressor_model.php

--- models/Compressor_model.php
+++ models/Compressor_model.php
@@ -11,16 +11,21 @@ class Compressor_model extends App_Model
      }
 
     /**
-     * Get compressor/s
-     * @param mixed $id compressor id
+     * Get comporessor/s
+     * @param mixed $id comporessor id
      * @param array $where perform where
      * @return mixed
      */
     public function get($id = '',  $where = [])
     {
-        
-        $this->db->select('*,' . db_prefix() . 'compressor.id');
-        $this->db->from(db_prefix() . 'compressor');
+        if (is_numeric($id)) {
+            $this->db->where('staffid', $id);
+            $category = $this->db->get(db_prefix() . 'comporessor')->row();
+
+            return $category;
+        }
+        $this->db->select('*,' . db_prefix() . 'comporessor.id');
+        $this->db->from(db_prefix() . 'comporessor');
         $this->db->where($where);
         $results = $this->db->get()->result_array();
         return $results;
@@ -29,10 +34,10 @@ class Compressor_model extends App_Model
 
     public function create($data){
         $data['regulasi'] = get_option('predefined_regulation_of_paa');
-        $this->db->insert(db_prefix().'compressor', $data);
+        $this->db->insert(db_prefix().'comporessor', $data);
         $equipment_id = $this->db->insert_id();
 
-        hooks()->do_action('after_compressor_added', $equipment_id);
+        hooks()->do_action('after_comporessor_added', $equipment_id);
         return $equipment_id;
     }
 
@@ -46,21 +51,40 @@ class Compressor_model extends App_Model
             $this->db->where('task_id', $task_id);
             $this->db->update(db_prefix() . $equipment_type, $data);
         }else{
+            $data['rel_id'] = $rel_id;
             $this->db->insert(db_prefix(). $equipment_type, $data);
         }
     }
 
     public function update($data, $rel_id, $task_id){
-        $field_nama_pesawat = $data['field'];
+        $field = $data['field'];
         unset($data['field']);
-        $data_nama_pesawat = htmlspecialchars($data['text'], ENT_QUOTES);
+        //$data_text = htmlspecialchars($data['text'], ENT_QUOTES);
+        $data_text = strip_tags($data['text'], '<div><p><br>');
+        //$data_text = $data['text'];
         unset($data['text']);
-        $data[$field_nama_pesawat] = $data_nama_pesawat;
+        $data[$field] = $data_text;
 
         $this->db->select('id');
         $this->db->where('rel_id', $rel_id);
         $this->db->where('task_id', $task_id);
-        $this->db->update(db_prefix() . 'compressor', $data);
+        $this->db->update(db_prefix() . 'comporessor', $data);
     }
 
+    public function update_pengujian_data($data){
+        $rel_id = $data['rel_id'];
+        $task_id = $data['task_id'];
+        $jenis_pesawat = strtolower($data['jenis_pesawat']);
+        $this->db->where('rel_id', $rel_id);
+        $this->db->where('task_id', $task_id);
+        $this->db->set($data['pengujian'], $data['value']);
+
+        unset($data['value']);
+        unset($data['pengujian']);
+        unset($data['jenis_pesawat']);
+        unset($data['rel_id']);
+        unset($data['task_id']);
+        
+        $this->db->update(db_prefix() . $jenis_pesawat, $data);
+    }
 }
