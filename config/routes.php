bbfb78e87d2591583c76387cc04070bdbd86ab85
4ndr1sw <4ndr1sw@gmail.com>
2022-09-17 09:12:52 +0700

    Update routes.php

--- config/routes.php
+++ config/routes.php
@@ -10,9 +10,11 @@ defined('BASEPATH') or exit('No direct script access allowed');
 $route['inspections/list'] = 'myinspection/list';
 $route['inspections/sticker/(:num)/html/(:num)/(:any)'] = 'myinspection/sticker/$1/$2/$3';
 $route['inspections/sticker/(:num)/pdf/(:num)'] = 'myinspection/sticker_data/$1/$2';
-$route['inspections/bapr/(:num)/html/(:any)'] = 'myinspection/bapr/$1/$2';
-$route['inspections/bapr/pdf/(:num)'] = 'myinspection/pdf/$1';
 
 $route['inspections/bapr/(:num)/html/(:num)/(:any)'] = 'myinspection/bapr_item/$1/$2/$3';
 $route['inspections/bapr/(:num)/pdf/(:num)'] = 'myinspection/item_pdf/$1/$2';
+
+$route['inspections/bapr/(:num)/html/(:any)'] = 'myinspection/bapr/$1/$2';
+$route['inspections/bapr/pdf/(:num)'] = 'myinspection/pdf/$1';
+
 $route['inspections/bapr/(:num)/template_pdf/(:num)'] = 'myinspection/raw_pdf/$1/$2';
