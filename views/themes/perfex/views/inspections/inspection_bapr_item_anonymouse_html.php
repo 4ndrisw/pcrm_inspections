cca8fc596dab95261080e04b706d31b1a76de215
4ndr1sw <4ndr1sw@gmail.com>
2022-09-28 15:45:42 +0700

    Update inspection_bapr_item_anonymouse_html.php

--- views/themes/perfex/views/inspections/inspection_bapr_item_anonymouse_html.php
+++ views/themes/perfex/views/inspections/inspection_bapr_item_anonymouse_html.php
@@ -37,17 +37,7 @@
          <div class="row">
             <div class="col-md-12">
                <h1 class="bold inspection-html-number"><?php echo format_inspection_item_number($inspection->id, $inspection->task_id); ?></h1>
-               <address class="inspection-html-company-info">
-                  <?php echo format_organization_info(); ?>
-               </address>
             </div>
-               <?php if(is_client_logged_in() || is_staff_logged_in()) { ?>
-                  <p class="bold"><?php echo $client_company; ?></p>
-                  <p class="no-mbot inspection-html-project">
-                     <span class="bold"><?php echo _l('project'); ?>:</span>
-                     <?php echo get_project_name_by_id($inspection->project_id); ?>
-                  </p>
-               <?php } ?>
                <p class="no-mbot inspection-html-date">
                   <span class="bold">
                   <?php echo _l('inspection_data_date'); ?>:
@@ -63,7 +53,6 @@
 
             <div class="row mtop25">
                <div class="col-md-12">
-                     <div class="bold"><?php echo get_option('invoice_company_name'); ?></div>
                      <div class="qrcode text-center">
                         <img src="<?php echo site_url('download/preview_image?path='.protected_file_url_by_path(get_inspection_upload_path('inspection').$inspection->id.'/assigned-'.$inspection_number.'.png')); ?>" class="img-responsive center-block inspection-assigned" alt="inspection-<?= $inspection->id ?>">
                      </div>
@@ -79,14 +68,3 @@
       </div>
    </div>
 </div>
-
-<?php
-   if($identity_confirmation_enabled == '1' && $can_be_accepted){
-    get_template_part('identity_confirmation_form',array('formData'=>form_hidden('inspection_action',4)));
-   }
-?>
-<script>
-   $(function(){
-     new Sticky('[data-sticky]');
-   })
-</script>
